f32bbd5a0706cd4c72422c9810e93428f969f6c2
Sannith <108894286+SannithDevoloper@users.noreply.github.com>
2022-10-11 14:10:42 +0530

    resume

---
+++ resume/src/App.js
@@ -0,0 +1,261 @@
+import './App.css'
+function App() {
+  return (
+    <div >
+       <nav class="navbar navbar-inverse "   >
+            <div class="container-fluid" >
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>                        
+                  .
+                </button>
+                
+               
+              </div>
+              <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav" >
+                    <li><a href="#Details">Profile</a></li>
+                  
+                  <li><a href="#Skills">Skills</a></li>
+                 
+                  <li><a href="#Education">Education</a></li>
+                  <li><a href="#Projects">Projects</a></li>
+                  <li><a href="#Hobbies">Hobbies</a></li>
+                </ul>
+              
+              </div>
+            </div>
+          </nav>
+        <div class="main container">
+        
+<div id="Details"  class="info">
+        
+            <div class="pic">
+                <img  src="https://i.ibb.co/WvTVQMk/Whats-App-Image-2022-03-14-at-7-57-44-PM.jpg" alt="Dp"/>
+        
+            </div>
+            <p> Hi..  this is Sannith i'm from india, i love developing the applications(FrontEnd),i am quick learner And always interested to learn latest technologies </p>
+           
+        
+        <hr/>
+        <p>
+            Done graduation(UG) in Computer Science in Institute Of Aeronautical Engineering at Hyderabad
+        </p>
+        <hr/>
+        <h4>Social Media</h4>
+        <p>
+            <a href="https://www.facebook.com/sannith.peddolla.1/"   target="blank">
+                <img src="https://i.ibb.co/GRZx3D5/facebook.png" alt="fb"
+                class="socialImg"/>
+            </a>
+            <a href="https://www.instagram.com/sannithreddi03/" target="blank">
+                <img src="https://i.ibb.co/19H5LvT/insta.png" alt="insta"
+                class="socialImg"/>
+            </a>
+            <a href="https://www.youtube.com/channel/UC4Gg1gvpLGMJD3aXjEVKT1g" target="blank">
+                <img src="https://i.ibb.co/w07K2Vn/youtube1.png" alt="youtube"
+                class="socialImg"/>
+            </a>
+        </p>
+        
+    
+    
+     
+</div>
+
+
+
+  <section id="Skills">
+<div class="Skills">
+<h3 > Skills :-</h3>
+<span>
+
+        <img src="https://i.ibb.co/LnbJRHM/html.png" alt="html" title="html"
+        class="skillImg"/>
+        <img  src="https://i.ibb.co/mHmg5WR/css.jpg" alt="css"  title="css"
+        class="skillImg"/>
+        <img  src="https://i.ibb.co/8cxQD2n/bootstrap.png" alt="bootstrap"  title="bootstrap" 
+        class="skillImg"/>
+         <img  src="https://i.ibb.co/zNKxdSs/Javascript.png" alt="Javascript"  title="Javascript"
+            class="skillImg"/>
+        <img src="https://i.ibb.co/zJYdJj9/react-js.jpg" alt="react-js"  title="react-js" 
+            class="skillImg"/>
+            <img  src="https://i.ibb.co/0mGkPYX/microsoft-sql-server.png" alt="microsoft-sql-server"  title="microsoft-sql-server" 
+            class="skillImg"/>
+            <img src="https://i.ibb.co/sqnHk7T/java.jpg" alt="java"  title="java"
+            class="skillImg"/>
+            <img src="https://i.ibb.co/zVK0kgk/informatica-data-integration-tools.png" alt="informatica-data-integration-tools" 
+            class="skillImg"/> 
+            <img src="https://i.ibb.co/4R17q06/pgsql.jpg" alt="pgsql"  title="pgsql"
+            class="skillImg"/> 
+            <img  src="https://i.ibb.co/rMkfQv3/python.jpg" alt="python" title="python" 
+            class="skillImg"/>
+            <img src="https://i.ibb.co/0mjmTM3/nodejs.jpg" alt="nodejs" title="node-js"
+            class="skillImg"/>
+            </span>
+</div>
+</section>
+<section id="Education">
+<div class="Education">
+    <h3 >Education Details :- </h3>
+    
+    
+    
+    <div class="table-responsive"> 
+    <table class="table table-hover table-bordered">
+        <thead>
+            <tr >
+                <th>Course</th>
+                <th>Institute</th>
+                <th>Pass out Year</th>
+                <th>Percentage</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Btech</td>
+                <td>Institute of Aeronautical Engineering</td>
+                <td>2019</td>
+                <td>70</td>
+            </tr>
+            <tr>
+                <td>Inter</td>
+                <td>Narayana Jr college </td>
+                <td>2015</td>
+                <td>92</td>
+            </tr>
+            <tr>
+                <td>SSC</td>
+                <td>R.B.V.R.R High school </td>
+                <td>2013</td>
+                <td>80</td>
+            </tr>
+        </tbody>
+    </table>
+    
+    </div>
+    </div>
+    </section>
+    
+
+<section id="Projects">
+<div class="projects">
+    <h3 >Project Details :-</h3>
+
+    <div id="projectdetails">
+        <div class="projectdesc">
+           <span >Project1</span>
+           <p >FoodDeliveryApp</p>
+
+        </div>
+        <div class="projectdesc">
+            <span >Project2</span>
+            <p >OrganicProductsApp</p>
+        </div>
+
+        <div class="project1">
+       <a href="https://sannith.netlify.app/" target="_blank"> <img class="img" src="https://i.ibb.co/99CqnrF/Screenshot-2.png" alt="Screenshot-2" />
+        <img class="img" src="https://i.ibb.co/Y7KkdSC/Screenshot-5.png" alt="Screenshot-5"  /></a>
+        </div>
+        <div class="project2">
+            <a href="https://organiccrops.netlify.app/" target="_blank">
+        <img class="img" src="https://i.ibb.co/G2f3jBR/Screenshot-8.png" alt="Screenshot-8"  />
+        <img class="img" src="https://i.ibb.co/jWKX8yL/Screenshot-9.png" alt="Screenshot-9"  />
+        </a>
+    
+        </div>
+        
+    </div>
+   
+
+</div>
+</section>
+<section id="Hobbies">
+    <div class="Hobbies">
+        <h3 >My Hobbies :-</h3>
+        
+        <div id="hobbiesmain">
+            <div class="hobbiescontainer">
+                <header>
+                    <p>Learning</p>
+                </header>
+                <img src="https://i.ibb.co/zZfphYb/learning.jpg" alt="learning"  class="hobbiesimg"/>
+            </div>
+            <div class="hobbiescontainer">
+                <header>
+                    <p>Listening Music</p>
+                </header>
+                <img src="https://i.ibb.co/qsJyHVK/listening.jpg" alt="listening"  class="hobbiesimg" />
+            </div>
+            <div class="hobbiescontainer">
+                <header>
+                    <p>Watching Movies</p>
+                </header>
+                <img src="https://i.ibb.co/FHfVfpx/watchingmovie.jpg" alt="watchingmovie"   class="hobbiesimg"/>
+            </div>
+            <div class="hobbiescontainer">
+                <header>
+                    <p>Playing volleyball</p>
+                </header>
+            <img src="https://i.ibb.co/t3SXp1v/volleyball.jpg" alt="volleyball"   class="hobbiesimg"/>
+            </div>
+            <div class="hobbiescontainer">
+                <header>
+                    <p>Travelling</p>
+                </header>
+                <img src="https://i.ibb.co/2FnQLxd/travel.jpg" alt="travel"  class="hobbiesimg"/>
+            </div>
+            <div class="hobbiescontainer">
+                <header>
+                    <p>Cooking</p>
+                </header>
+                <img src="https://i.ibb.co/c2PPLry/cooking.jpg" alt="cooking"  class="hobbiesimg"/>
+            </div>
+        </div>
+       
+
+    </div>
+</section>
+</div>
+<div id="devCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#devCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#devCarousel" data-slide-to="1"></li>
+                <li data-target="#devCarousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img src="https://i.ibb.co/zZfphYb/learning.jpg" alt="ft" title="reading" class="slideImage" />
+                </div>
+                <div class="item">
+                    <img src="https://i.ibb.co/qsJyHVK/listening.jpg" alt="ft" title="listening songs" class="slideImage"/>
+                </div>
+                <div class="item">
+                    <img src="https://i.ibb.co/FHfVfpx/watchingmovie.jpg" alt="ft" title="watchingMovie"class="slideImage"/>
+                </div>
+            </div>
+            <a class="left carousel-control" href="#devCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#devCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+
+        </div>
+    
+    <footer>
+        <div class="footer">
+             <p>&copy; 2022 My Resume </p>
+             <p>Created By sannith </p>
+            </div>
+        
+    </footer>
+    
+    </div>
+   )
+  
+}
+
+export default App;
